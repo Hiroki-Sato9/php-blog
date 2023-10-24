0b434d1ef6baf0763d6ee3f149fb67ab8dc1577b
Hiroki-Sato9 <resumi30minutes@gmail.com>
2023-10-24 10:42:43 +0900

    change order

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
     public function index(Post $post) {
-        return $post->get();
+        
+        return view('posts/index')
+            ->with(['posts' => $post
+            ->orderByDesc('updated_at')->limit(10)->get()]);
     }
 }
