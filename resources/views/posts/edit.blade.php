2ab43d39aca9be12296614fd73e0ccb5c91fea40
Hiroki-Sato9 <resumi30minutes@gmail.com>
2023-10-27 14:26:20 +0900

    ブログ投稿編集画面の作成

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body class="antialiased">
+        <form action="/posts" method="patch">
+        @csrf
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" value="{{ $post->title }}" />
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]">{{ $post->body }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+            <input type="submit" value="保存">
+        </form>
+        <div class="footer">
+            <a href={{ url("/posts/"); }}>元に戻る</a>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,6 @@ Route::get('/', [PostController::class, 'index']);
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 
