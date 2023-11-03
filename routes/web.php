5fa579b699d7cf39ce52e74511fb341582debe7c
Hiroki-Sato9 <resumi30minutes@gmail.com>
2023-11-03 10:49:08 +0900

    ヘッダーのナビゲーションの修正

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/', [PostController::class, 'index'])->name('index');
+
 Route::put('/posts/{post}', [PostController::class, 'update']);
-
-Route::get('/', [PostController::class, 'index']);
-
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::post('/posts', [PostController::class, 'store']);
@@ -27,4 +29,21 @@ Route::delete('/posts/{post}', [PostController::class, 'delete']);
 
 
 Route::get('/categories/{category}', [CategoryController::class, 'index']);
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
